52c531dbda59df9032d8f99c8fa2ab6fc996c30f
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2024-03-21 23:18:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const b = fs.readFileSync('index.html', 'utf8');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
